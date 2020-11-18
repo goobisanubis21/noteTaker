fd68f06e72e43d0d425ea11b0ec29f946c3322a5
Anthony Parrino <anthony21parrino21@gmail.com>
2020-11-17 21:57:36 -0500

    added ids to notes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var path = require("path");
 var fs = require("fs");
 var db = require("./db/db.json");
+var { v4: uuidv4 } = require("uuid");
+
 
 // Sets up the Express App
 var app = express();
@@ -21,19 +23,22 @@ app.get("/notes", function (req, res) {
 });
 
 app.get("/api/notes", function (req, res) {
-    fs.readFile("db/db.json", 'utf8', function(err, data) {
+    fs.readFile("db/db.json", 'utf8', function (err, data) {
         res.json(data);
     });
 });
 
 app.post("/api/notes", function (req, res) {
-    var newNote = req.body;
-    fs.readFile("./db/db.json", function(err, data) {
+    var title = req.body.title;
+    var text = req.body.text;
+    var newNote = { title, text, id: uuidv4() };
+    fs.readFile("./db/db.json", function (err, data) {
         var currentNotes = JSON.parse(data);
         currentNotes.push(newNote);
-        fs.writeFile("./db/db.json", [JSON.stringify(currentNotes)], function(err) {
+        fs.writeFile("./db/db.json", [JSON.stringify(currentNotes)], function (err) {
             console.log(newNote)
         })
+        res.sendFile(path.join(__dirname, "/db/db.json"));
     })
 
 });
